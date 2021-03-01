2fe05fac7d82442e7ab284e49c6e7d3acbea93bf
Teddir <ferryaja20000@gmail.com>
2021-03-01 13:15:48 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
  <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+    /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
+    /*
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
@@ -14,61 +14,60 @@
 */
 
 
-// generate app_key
-// $router->get('/key', function() {
-//     return Illuminate\Support\Str::random(36);
-// });
+    // generate app_key
+    // $router->get('/key', function() {
+    //     return Illuminate\Support\Str::random(36);
+    // });
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+    $router->get('/', function () use ($router) {
+        return $router->app->version();
+    });
 
-// API route group
-$router->group(['prefix' => 'api'], function () use ($router) {
-    // Matches "/api/register
-    $router->post('register', 'AuthController@register');
-    $router->post('register/kasirs', 'AuthController@registerKasir');
-     // Matches "/api/login
-    $router->post('login', 'AuthController@login');
-    $router->post('login/kasirs', 'AuthController@loginKasir');
-
-    $router->group(['middleware' => 'auth.jwt'], function () use ($router) {
+    // API route group
+    $router->group(['prefix' => 'api'], function () use ($router) {
+        // Matches "/api/register
+        $router->post('register', 'AuthController@register');
+        $router->post('register/kasirs', 'AuthController@registerKasir');
+        // Matches "/api/login
+        $router->post('login', 'AuthController@login');
+        $router->post('login/kasirs', 'AuthController@loginKasir');
 
         $router->get('users/all', 'AuthController@getAllUser');
-        $router->get('users/One/{id}', 'AuthController@getOneUser');
-        $router->get('users/Role/{id}', 'AuthController@getRoleUser'); //---->role ada  2
-        $router->put('users/update/{id}', 'AuthController@update');
-        
+        $router->group(['middleware' => 'auth.jwt'], function () use ($router) {
 
-        $router->get('suppliers', 'SupplierController@index');
-        $router->post('/suppliers/create', 'SupplierController@store');
-        $router->put('/suppliers/update/{id}', 'SupplierController@update');
-        $router->delete('/suppliers/delete/{id}', 'SupplierController@delete');
+            $router->get('users/One/{id}', 'AuthController@getOneUser');
+            $router->get('users/Role/{id}', 'AuthController@getRoleUser'); //---->role ada  2
+            $router->put('users/update/{id}', 'AuthController@update');
 
-        $router->get('kategoris', 'KategoriController@index');
-        $router->post('/kategoris/create', 'KategoriController@store');
-        $router->put('/kategoris/update/{id}', 'KategoriController@update');
-        $router->delete('/kategoris/delete/{id}', 'KategoriController@delete');
 
-        $router->get('barangs', 'BarangController@index');
-        $router->post('/barangs/create', 'BarangController@store');
-        $router->put('/barangs/update/{id}', 'BarangController@update');
-        $router->delete('/barangs/delete/{id}', 'BarangController@delete');
+            $router->get('suppliers', 'SupplierController@index');
+            $router->post('/suppliers/create', 'SupplierController@store');
+            $router->put('/suppliers/update/{id}', 'SupplierController@update');
+            $router->delete('/suppliers/delete/{id}', 'SupplierController@delete');
 
-        $router->get('expenses', 'ExpenseController@index');
-        $router->post('/expenses/create', 'ExpenseController@store');
-        $router->put('/expenses/update/{id}', 'ExpenseController@update');
-        $router->delete('/expenses/delete/{id}', 'ExpenseController@delete');
+            $router->get('kategoris', 'KategoriController@index');
+            $router->post('/kategoris/create', 'KategoriController@store');
+            $router->put('/kategoris/update/{id}', 'KategoriController@update');
+            $router->delete('/kategoris/delete/{id}', 'KategoriController@delete');
 
-        $router->get('transaksis', 'TransaksiController@index');
-        $router->post('/transaksis/create', 'TransaksiController@store');
-        $router->put('/transaksis/update/{id}', 'TransaksiController@update');
-        $router->delete('/transaksis/delete/{id}', 'TransaksiController@delete');
+            $router->get('barangs', 'BarangController@index');
+            $router->post('/barangs/create', 'BarangController@store');
+            $router->put('/barangs/update/{id}', 'BarangController@update');
+            $router->delete('/barangs/delete/{id}', 'BarangController@delete');
 
-        $router->get('buys', 'BuyController@index');
-        $router->post('/buys/create', 'BuyController@store');
-        $router->put('/buys/update/{id}', 'BuyController@update');
-        $router->delete('/buys/delete/{id}', 'BuyController@delete');
+            $router->get('expenses', 'ExpenseController@index');
+            $router->post('/expenses/create', 'ExpenseController@store');
+            $router->put('/expenses/update/{id}', 'ExpenseController@update');
+            $router->delete('/expenses/delete/{id}', 'ExpenseController@delete');
+
+            $router->get('transaksis', 'TransaksiController@index');
+            $router->post('/transaksis/create', 'TransaksiController@store');
+            $router->put('/transaksis/update/{id}', 'TransaksiController@update');
+            $router->delete('/transaksis/delete/{id}', 'TransaksiController@delete');
+
+            $router->get('buys', 'BuyController@index');
+            $router->post('/buys/create', 'BuyController@store');
+            $router->put('/buys/update/{id}', 'BuyController@update');
+            $router->delete('/buys/delete/{id}', 'BuyController@delete');
+        });
     });
-});
-
